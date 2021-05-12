98787f92ca843611fce057e94dc2d60aea7db2d9
omair daleen <omairdal@gmail.com>
2021-05-12 22:25:19 +0300

    adjusting the geo of Entry

--- idfk.py
+++ idfk.py
@@ -1,11 +1,13 @@
 from tkinter import* 
 
 root = Tk()
-root.geometry("500x400+700+150")
+root.geometry("403x400+700+150")
 
 first = Entry(root, width=60)
 second = Entry(root, width=60)
-first.grid(row=1, column=0, padx=20, columnspan=3)
-second.grid(row=0, column=0, padx= 20, pady=10, columnspan=3)
+first.grid(row=1, column=0, padx=20,pady=5, ipady=15, columnspan=3)
+second.grid(row=0, column=0, padx= 20, pady=5, columnspan=3)
+
+
 
 root.mainloop()
